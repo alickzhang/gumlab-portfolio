1950065227bec97c1613b0222ec6280bea522946
Nathan Fang <nathan.fang@lmigroup.com>
2018-01-23 14:59:50 +1100

    epic: project setup

---
+++ data/SiteConfig.js
@@ -0,0 +1,30 @@
+module.exports = {
+  projectDir: "projects", // The name of directory that contains your posts.
+  siteTitle: "Wen Shi's Portfolio", // Site title.
+  siteTitleAlt: "Digital Agency Portfolio", // Alternative site title for SEO.
+  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
+  siteUrl: "https://nfang.github.io/portfolio/", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription: "Portfolio website for Wen Shi", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteFBAppID: "", // FB Application ID for using app insights
+  googleAnalyticsID: "", // GA tracking ID.
+  disqusShortname: "", // Disqus shortname.
+  postDefaultCategoryID: "Tech", // Default category for posts.
+  userName: "Wen Shi", // Username to display in the author segment.
+  userTwitter: "WEN__SHI", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Melbourne, Australia", // User location to display in the author segment.
+  userAvatar: "https://media-exp2.licdn.com/media/AAEAAQAAAAAAAALgAAAAJDE1NWMyMjcwLTAyMjItNDQzNy04NjU4LWMwZjIyZTNjNzc4MA.jpg", // User avatar to display in the author segment.
+  userDescription: "", // User description to display in the author segment.
+  // Links to social profiles/projects you want to display in the author segment/navigation bar.
+  userLinks: [
+    {
+      label: "Twitter",
+      url: "https://twitter.com/WEN__SHI",
+      iconClassName: "fa fa-twitter"
+    }
+  ],
+  copyright: "Copyright © 2018. Wen Shi", // Copyright string for the footer of the website and RSS feed.
+  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#e0e0e0" // Used for setting manifest background color.
+};
