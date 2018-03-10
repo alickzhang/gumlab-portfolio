e550cf5f660a8167c028efd90306fd7f43551275
Nathan Fang <nathan.fang@icloud.com>
2018-03-10 18:13:05 +1100

    fix: pathPrefix is not allowed to be empty

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,7 +4,7 @@ module.exports = {
   siteTitleAlt: "Gumlab Digital Agency Portfolio", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://gumlab-portfolio.netlify.com", // Domain of your website without pathPrefix.
-  pathPrefix: "", // Prefixes all links.
+  pathPrefix: "/", // Prefixes all links.
   siteDescription: "Portfolio website for Gumlab", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   googleAnalyticsID: "", // GA tracking ID.
