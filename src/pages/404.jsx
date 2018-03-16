948b8687f3508391f84f29cf009b8cefce56c27f
Alick Zhang <alick.zhang@lmigroup.com>
2018-03-16 11:50:41 +1100

    fix #38: page title of 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,10 +5,12 @@ import config from "../../data/SiteConfig";
 class NotFoundPage extends Component {
   render() {
     return (
-      <div className="about-container">
-        <Helmet title={`About | ${config.siteTitle}`} />
-        <h1>404 NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div>
+        <Helmet title={`Page not found | ${config.siteTitle}`} />
+        <div style={{ textAlign: 'center' }}>
+          <h1>404 NOT FOUND</h1>
+          <p>We can&#39;t find the page you are looking for.</p>
+        </div>
       </div>
     );
   }
